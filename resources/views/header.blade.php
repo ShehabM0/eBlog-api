60cdb1b1fc0f1014cf392cceb707fc0f27d14a4a
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-02 14:11:12 +0200

    added pages

---
+++ resources/views/header.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{{asset("css/header.css")}}">
+    <link rel="stylesheet" href="{{asset("css/home.css")}}">
+    <link rel="stylesheet" href="{{asset("css/card.css")}}">
+    <link rel="stylesheet" href="{{asset("css/alert.css")}}">
+    <link rel="stylesheet" href="{{asset("css/auth.css")}}">
+    <link rel="stylesheet" href="{{asset("css/post.css")}}">
+    <link rel="stylesheet" href="{{asset("css/create.css")}}">
+    <link rel="stylesheet" href="{{asset("css/edit.css")}}">
+    <link rel="stylesheet" href="{{asset("css/myposts.css")}}">
+    <title>eBlog</title>
+
+    <!-- fonts -->
+    <link 
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+    />
+    <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com" crossorigin 
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <script src="{{asset("js/app.js")}}"></script>
+  </head>
+  <body>
+
+    <!-- page header  -->
+    <div class="header">
+      <div class="nav-container">
+        @guest
+        <a href="/blog/">
+          <span class="logo">
+              e<span>B</span>log
+          </span>
+        </a>
+        <span>
+            <a href="/blog/pages/auth/signup.php" class="signup">SignUp</a>
+            <a href="/blog/pages/auth/login.php" class="login">Login</a>
+        </span>
+        @endguest
+
+        @auth
+          <div class="left-side">
+            <a href="/blog/">
+              <span class="logo"> e<span>B</span>log </span>
+            </a>
+            <button id="create-post">Create Post</button>
+            <a href="/blog/pages/posts/myposts.php"><button id="create-post">My posts</button></a>
+          </div>
+          <span>
+              <span style="color: #fff; padding: 0 15px">Hello User</span>
+              <a href="/blog/control/auth/logout.php" class="logout">Logout</a>
+          </span>
+        @endauth
+      </div>  
+    </div>  
+
+    <!-- create post window  -->
+    <div class="create-modal modal-container">
+      <div class="create-modal modal-header">
+        <button class="close-btn" id="create-close-btn">&times;</button>
+      </div>
+      <div class="create-modal modal-body">
+        <form action="/blog/control/posts/post.php" method="POST" enctype="multipart/form-data">
+          <!-- title -->
+          <label for="title">
+            Title
+            <span style="color: red">*</span>
+          </label>
+          <input
+            type="text"
+            id="title"
+            placeholder="type the post title.."
+            name="title"
+          />
+          <!-- image -->
+          <label for="image">
+            Image link
+            <span style="color: red">*</span>
+          </label>
+          <input type="file"  name="image" />
+          <!-- body  -->
+          <label for="body">
+            Body
+            <span style="color: red">*</span>
+          </label>
+          <textarea
+            type="text"
+            id="text-body"
+            placeholder="type the post body.."
+            name="body"
+          ></textarea>
+          <!-- buttons -->
+          <input
+            type="submit"
+            value="Create"
+            id="submit-button"
+            class="buttons"
+            name="post-create-form"
+          />
+        </form>
+      </div>
+    </div>
+
+    <!-- page alerts   -->
+      <div class="alert-msg">
+        <button class="close-btn">&times;</button>
+        <p>An alert message!</p>
+      </div>
---
+++ resources/views/posts/myposts.blade.php
@@ -0,0 +1,3 @@
+    @extends('header')
+  </body>
+</html>
