ee42b70fefbf7172fe25d36b2ea50fc08cee105c
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-04 15:58:28 +0200

    edited form variables

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -6,13 +6,15 @@
           </div>
         </div>
         <div class="form-container">
-          <form action="#" method="POST">
+          <form action="/signup" method="POST">
+            <!-- avoid 419 page expired error (token verification failure) -->
+            @csrf
             <h2>Create an account</h2>
             <input
               type="text"
               placeholder="Username"
               required="required"
-              name="username"
+              name="name"
             />
             <input
               type="email"
@@ -30,14 +32,14 @@
               type="password"
               placeholder="Confirm Password"
               required="required"
-              name="password_confirm"
+              name="password_confirmation"
             />
             <div class="p-class">
               <input
                 type="submit"
                 id="submit-btn"
                 value="Create Account"
-                name="regform"
+                name="signupform"
               />
               <p>Already have an account?<a href="{{asset("login")}}">Login</a></p>
             </div>
