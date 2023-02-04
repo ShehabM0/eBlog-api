6f48da279b8a16bd9e2252f139f833dfb4ea04a0
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-04 16:01:01 +0200

    added signup authController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    
+    public function signupPage() {
+        return view("auth.signup");
+    }
+
+    public function loginPage() {
+        return view("auth.login");
+    }
+
+    public function signup(Request $req) {
+        $user = $req->validate([
+            'name' => 'required|string|min:3|max:20',
+            'email' => 'required|regex:/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/|unique:users',
+            'password' => 'required|confirmed|min:8'
+        ]);
+        $user['password'] = Hash::make($user['password']);
+        User::create($user);
+        return redirect("/login")->with("message", "Account Created Successfully.");
+    }
 }
