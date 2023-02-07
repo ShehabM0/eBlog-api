9644d13a36e2812dfa224174b85103e9d535d60e
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-07 11:20:34 +0200

    added create-message function

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    
+    public function __construct()
+    {
+        // in case not logged-in user trying to create or edit (unauthenticated)
+        $this->middleware("auth");
+    }
+
+    public function create(Request $req) {
+        $comment = $req->validate([
+            'comment' => 'required|max:255',
+            'post_id' => 'required|numeric'
+        ]);
+        $post_exist = Post::find($comment["post_id"]);
+        // in case user changed post_id from console
+        if(!$post_exist)
+            return redirect("/")->with("message", "The post you are trying to access doesn't exist!");
+        // no need to check for user auth as the middleware does
+        $comment['user_id'] = Auth::id();
+        Comment::create($comment);
+        return redirect('/post/'.$post_exist->id)->with("message", "Comment Added Successfully.");
+    }
 }
