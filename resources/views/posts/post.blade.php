4c57b28186c8ccbed7c05c830f1bb96203e13490
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-07 10:07:27 +0200

    added Comments/Categories section to post-page

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -36,6 +36,99 @@
         </div>
       @endif
 
+      <!-- Comments and Categories Section -->
+      <div class="comments-category">
+          <div class="comments-container">
+
+            <div class="comments-section-name">
+              <h3>Comments</h3>
+              <img src="{{asset("img/bx-message-square.svg")}}" alt="">
+            </div>
+
+            <!-- post has no comments -->
+            <div class="no-comments">
+              <h3>Be the first to leave a comment</h3>
+            </div>
+            
+            <div class="comment">
+              <div class="comment-user">
+                <div class="ls"">
+                  <img src="{{asset("img/bx-user.svg")}}" alt="">
+                  <h4>Shehab Mohamed</h4>
+                </div>
+                <div class="rs">
+                  <form action="/comment/delete" method="POST">
+                    <a href="#"> Delete </a>
+                  </form>
+                </div>
+              </div>
+              <div class="comment-text">
+                <p>Lorem Ipsum is simply dummy text of</p>
+              </div>
+            </div>
+
+            <div class="comment">
+              <div class="comment-user">
+                <div class="ls"">
+                  <img src="{{asset("img/bx-user.svg")}}" alt="">
+                  <h4>Shehab Mohamed</h4>
+                </div>
+                <div class="rs">
+                  <form action="/comment/delete" method="POST">
+                    <a href="#"> Delete </a>
+                  </form>
+                </div>
+              </div>
+              <div class="comment-text">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+              </div>
+            </div>
+
+            <div class="comment">
+              <div class="comment-user">
+                <div class="ls"">
+                  <img src="{{asset("img/bx-user.svg")}}" alt="">
+                  <h4>Shehab Mohamed</h4>
+                </div>
+                <div class="rs">
+                  <form action="/comment/delete" method="POST">
+                    <a href="#"> Delete </a>
+                  </form>
+                </div>
+              </div>
+              <div class="comment-text">
+                <p>Where can I get some?</p>
+              </div>
+            </div>
+            
+            <!-- login to leave a comment -->
+            <div class="login-comment">
+              <a href="/login">LogIn&nbsp;</a>
+              <h3>to Comment</h3>
+            </div>
+            <!-- add comment form section -->
+            <div class="comment-form">
+              <form action="/comment/delete" method="POST">
+                <textarea name="text" placeholder="What are your thoughts"></textarea>
+                <input type="submit" value="Comment">
+              </form>
+            </div>
+            
+          </div>
+          <!-- Categories section -->
+          <div class="category-container">
+            <h3 class="categories-section-name">Categories</h3>
+            <div class="categories">
+              <!-- post has no categories -->
+              <h3>No categories available<br> for this post</h3>
+              <!-- categories -->
+              <span>category1</span>
+              <span>category1</span>              
+            </div>
+          </div>
+        </div>
+
       <!-- edit post window -->
       <div class="edit-modal modal-container">
         <div class="edit-modal modal-header">
